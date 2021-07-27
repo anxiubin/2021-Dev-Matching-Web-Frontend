4fc58eeae6e293a2fff572f313fb58ca9a7b60a4
anxiubin <tnqls0120@naver.com>
2021-07-27 13:05:07 +0900

    ♻️ asset 모두 다른 무료 이미지로 변경

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,19 +1,19 @@
-export default function Loading ({$app, initialState}) {
-  this.state = initialState
-  
-  this.$target = document.createElement('div')
-  this.$target.className = "Loading Modal"
-  $app.appendChild(this.$target)
+export default function Loading({ $app, initialState }) {
+	this.state = initialState
 
-  this.setState = nextState => {
-      this.state = nextState
-      this.render()
-  }
+	this.$target = document.createElement("div")
+	this.$target.className = "Loading Modal"
+	$app.appendChild(this.$target)
 
-  this.render = () => {
-      this.$target.innerHTML = `<div class="content"><img src="./assets/nyan-cat.gif" /></div>`
-      this.$target.style.display = this.state ? 'block' : 'none'
-  }
+	this.setState = (nextState) => {
+		this.state = nextState
+		this.render()
+	}
 
-  this.render()
+	this.render = () => {
+		this.$target.innerHTML = `<div class="content"><img src="./assets/loading_cat.gif" /></div>`
+		this.$target.style.display = this.state ? "block" : "none"
+	}
+
+	this.render()
 }
--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -32,7 +32,7 @@ export default function Nodes({ $app, initialState, onClick, onBackClick }) {
 				.join("")
 
 			this.$target.innerHTML = !this.state.isRoot
-				? `<div class="Node"><img src="./assets/prev.png" /></div> ${nodesTemplate}`
+				? `<div class="Node"><img style="width: 100" src="./assets/prev.png" /></div> ${nodesTemplate}`
 				: nodesTemplate
 		}
 	}
